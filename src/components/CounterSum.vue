2c0822b9841f541e0bcf13e85555fcb3ad2d86b7
oujianfeng <247057911@qq.com>
2019-07-21 10:08:09 +0800

    create CounterSum component which accept a number as props to show sun of Counter

---
+++ src/components/CounterSum.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>{{ sum }}</span>
+</template>
+
+<script>
+export default {
+  name: "counterSum",
+  props: {
+    sum: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
